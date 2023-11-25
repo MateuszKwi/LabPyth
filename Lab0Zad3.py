790f29bdbdc5d5f023fe14f01545de9c519ae602
MateuszKwi <151962120+MateuszKwi@users.noreply.github.com>
2023-11-25 17:32:16 +0100

    Add files via upload

---
+++ Lab0Zad3.py
@@ -0,0 +1,9 @@
+a = float(input("Wpisz długość pierwszego boku prostokątach w CM: "))
+b = float(input("Wpisz długość drugiego boku prostokątach w CM: "))
+
+#pole prostokąta
+p = a * b
+#obwód prostokąta
+obw = 2 * a + 2 * b
+
+print(f"Pole prostokąta wynosi {p}cm^2, a obwód prostokąta wynosi {obw}cm")
