f34a004f44a03f049ba21af253d9a32537a4d77d
MateuszKwi <151962120+MateuszKwi@users.noreply.github.com>
2023-12-10 20:29:12 +0100

    cyk

---
+++ Lab2Zad10.py
@@ -0,0 +1,13 @@
+tekst="Rzeszów jest piękny"
+LiteraPierwsza = tekst[0]
+print("Pierwsza litera:", LiteraPierwsza)
+
+LiteraSiodma=tekst[6]
+LiteraDziewata=tekst[9]
+LiteraTrzynasta=tekst[12]
+LiteraDruga=tekst[1]
+
+print("Siódma litera:", LiteraSiodma)
+print("Dziesiąta litera:", LiteraDziewata)
+print("Trzynasta litera:", LiteraTrzynasta)
+print("Druga litera:", LiteraDruga)
