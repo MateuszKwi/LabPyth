f318c604e806fffb521330748123424837f5e7db
MateuszKwi <151962120+MateuszKwi@users.noreply.github.com>
2023-12-10 19:57:57 +0100

    cyk

---
+++ Lab2Zad5.py
@@ -0,0 +1,15 @@
+#a
+for i in range(1,101):
+    print(1, end=", " )
+
+#b
+for i in range(100,-1,-1):
+    print(1, end=", ")
+
+#c
+for i in range(7, 78, 7):
+    print(1, end=", ")
+
+#d
+for i in range(20, -1, -2):
+    print(1, end=", ")
