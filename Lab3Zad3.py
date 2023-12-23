21fd70cc28bf7d18c41d258b126cafed8bfd0d0b
MateuszKwi <151962120+MateuszKwi@users.noreply.github.com>
2023-12-23 13:56:44 +0100

    cyk

---
+++ Lab3Zad3.py
@@ -0,0 +1,29 @@
+tekst="Python jest super"
+
+#a
+
+print(tekst[0])
+#v1
+print(tekst[-1])
+#v2
+dl=len(tekst)
+print(tekst[dl-1])
+
+#b
+for i in range(0,dl,2):
+    print(tekst[i])
+
+#c
+for i in range(1,dl,3):
+    print(tekst[i])
+
+#d
+
+#v1
+for i in range(10,dl):
+    print(tekst[i])
+#v2
+print(tekst[10:])
+
+#e
+print(tekst[::-1])
