1ae3fbec5ceb7d97f2bf689429e3905b0ec30076
MateuszKwi <151962120+MateuszKwi@users.noreply.github.com>
2023-12-23 13:56:31 +0100

    cyk

---
+++ Lab3Zad2.py
@@ -0,0 +1,16 @@
+tekst="Rzeszów jest piękny"
+
+#a
+pierwsza_litera = tekst[0]
+print(f"a. Pierwsza litera: {pierwsza_litera}")
+
+#b
+siodma_litera=tekst[6]
+dziesiata_litera=tekst[9]
+trzynasta_litera=tekst[12]
+druga_litera=tekst[1]
+
+print(f"b. Siódma litera: {siodma_litera}")
+print(f"   Dziesiąta litera: {dziesiata_litera}")
+print(f"   Trzynasta litera: {trzynasta_litera}")
+print(f"   Druga litera: {druga_litera}")
